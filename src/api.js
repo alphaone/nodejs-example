41919710eb13390e66123cbf9e54bd80077eed09
Torsten Mangner <tmangner@inoxio.de>
2019-03-24 18:57:04 +0100

    Style page with SCSS

--- src/api.js
+++ src/api.js
@@ -3,11 +3,15 @@ const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 const httpStatusCodes = require('http-status-codes');
+const sass = require('node-sass-middleware');
+const path = require('path');
 const cors = require('cors');
 const health = require('./routes/health');
+
 const search = require('./routes/search');
 
 const response = require("./errors/response");
+
 
 class App {
 
@@ -20,15 +24,24 @@ class App {
 
     _setup() {
         this.express.set('view engine', 'pug');
+        this.express.use(
+            sass({
+                src: path.join(__dirname, '/../sass'),
+                dest: path.join(__dirname, '/../public/css'),
+                debug: true,
+                prefix: '/public/css'
+            })
+        );
+        this.express.use('/public', express.static(path.join(__dirname, '/../public')));
     }
-    
+
     _middleware() {
         this.express.use(logger('combined'));
         this.express.use(bodyParser.json());
         this.express.use(bodyParser.urlencoded({extended: false}));
         this.express.use(cors({optionsSuccessStatus: httpStatusCodes.OK}))
     }
-    
+
     _routes() {
         this.express.use('/health', health.getRouter());
         this.express.use('/search', search.getRouter());
@@ -36,13 +49,13 @@ class App {
         this.express.use(this._errorHandler.bind(this))
     }
 
-    _errorHandler (err, req, res, next) {
+    _errorHandler(err, req, res, next) {
         // bring the error into our format
         if (!(err instanceof response.ResponseError)) {
             err = new response.ServerError(err.message)
         }
         res.statusCode = err.code;
-        res.json({ code: err.code, time: (new Date()).toISOString(), message: err.message })
+        res.json({code: err.code, time: (new Date()).toISOString(), message: err.message})
     }
 }
 
--- src/controllers/search.js
+++ src/controllers/search.js
@@ -4,7 +4,7 @@ let interestingFields = ({trackName, collectionName, artistName, trackTimeMillis
     ({trackName, collectionName, artistName, trackTimeMillis, artworkUrl100});
 
 function search(query) {
-    return got(`https://itunes.apple.com/search?media=music&entity=album&term=${query}&limit=10`, { json: true }).then(response => {
+    return got(`https://itunes.apple.com/search?media=music&entity=album&term=${query}&limit=50`, { json: true }).then(response => {
         return response.body.results.map((r) => interestingFields(r));
     }).catch(error => {
         throw Error(`Got error while calling itunes: ${error.body}`);
